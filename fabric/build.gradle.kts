cb37d2e67bbe68fb4e30bbb86c471fbba41ba72d
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-08-04 14:26:52 -0700

    switched to modrinth and Tim Core 1.1.0

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -38,8 +38,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit_version")}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${property("junit_version")}")
 
-    // TOOD: Change to Modrinth maven.
-    modImplementation(files("libs/timcore-fabric-1.6.1-1.0.0.jar"))
+    modImplementation("maven.modrinth:cobblemon-tim-core:${property("tim_core_fabric_version")}")
 
     modImplementation("io.wispforest:owo-lib:${property("owo_version")}")
     annotationProcessor("io.wispforest:owo-lib:${property("owo_version")}")
